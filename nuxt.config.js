8c56452bcbee4613b9d5bdaa7b3b11aae48e0868
pierre golfier <pierre@codeur.com>
2020-06-20 23:29:41 +0200

    extend twitter card description

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { hid: 'og:image', name: 'og:image', property: 'og:image', content: 'https://twitchbuddy.app/icon.png' },
-      { hid: 'twitter:description', name: 'twitter:description', property: 'twitter:description', content: 'A Twitch.tv toolbox' }
+      { hid: 'twitter:description', name: 'twitter:description', property: 'twitter:description', content: 'A Twitch.tv toolbox for clips and replays' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
